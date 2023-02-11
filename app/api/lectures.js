0c89fd73adfa732367102198c6c8c7e0a4b0b007
Sanjay Ramanathan <sonjak737@gmail.com>
2023-02-10 18:00:46 -0800

    get and delete lectureId handler

--- app/api/lectures.js
+++ app/api/lectures.js
@@ -21,6 +21,12 @@ async function getCourse(courseId) {
     })
 }
 
+async function getLecture (lectureId) {
+    return await db.Lecture.findOne({
+        where: { id: lectureId }
+    })
+}
+
 async function getSectionsIdsFromCourse(courseId) {
     return await db.Section.findAll({
         where: {
@@ -29,6 +35,15 @@ async function getSectionsIdsFromCourse(courseId) {
         attributes: ['id']
     })
 }
+
+async function getSectionLectureRelation (lectureId, sectiondId) {
+    return await db.LectureForSection.findOne({
+        where: {
+            lectureId: lectureId,
+            sectionId: sectiondId
+        }
+    }
+)}
 
 // get all lecture objects for the current course
 router.get('/', async function (req, res) {
@@ -60,6 +75,7 @@ router.post('/', async function (req, res) {
     const enrollment = getEnrollment(user.id, courseId)
     var lecture     // will hold the returned lecture object from database
 
+    // TODO: how are we treating the role 'ta'?
     if (enrollment.role == 'teacher') {
         // create lecture object
         newLec = req.body
@@ -123,6 +139,87 @@ router.put('/:lecture_id', async function (req, res) {
     }
     else {      // if user is not a teacher
         res.status(403).send()
+    }
+})
+
+router.get('/:lecture_id', async function (req, res) {
+    const user = req.user
+    const lectureId = req.params.lecture_id
+    const enrollment = getEnrollment(user.id, courseId)
+    var full_response = {}  // will hold response with lecture info and related questions
+
+    if (enrollment.role == 'teacher') {     // if teacher, send lecture info & all related questions
+        try {
+            const lecture = getLecture(lectureId)
+            full_response.push(lecture)
+
+            const questions_in_lec = await db.sequelize.query(  // raw sql query to get all questions in this lecture using `QuestionInLecture`
+                'SELECT q.* FROM Question q INNER JOIN QuestionInLecture ql ON q.id = ql.questionId INNER JOIN Lecture l ON ql.lectureId = l.id'
+            )
+            full_response.push(questions_in_lec)
+        }
+        catch (e) {
+            // TODO: send back better error msg using exception 'e' (/lib/string_helpers.js - serializeSequelizeErrors())
+            res.status(400).send({error: "Unable to get lecture or questions"})
+        }
+        res.status(200).send(full_response)
+    }
+    else {  // if student, only send published info
+        sectionLectureRelation = getSectionLectureRelation(lectureId, enrollment.sectionId)
+        if (!sectionLectureRelation.published) {    // if this lecture (from user's section) isn't published
+            res.send(404).send()
+        }
+        else {  // if this lecture (from user's section) is published
+            try {
+                const lecture = getLecture(lectureId)
+                full_response.push(lecture)
+    
+                const questions_in_lec = await db.sequelize.query(  // raw sql query to get all published questions in this lecture using `QuestionInLecture`
+                    'SELECT q.* FROM Question q INNER JOIN QuestionInLecture ql ON q.id = ql.questionId INNER JOIN Lecture l ON ql.lectureId = l.id WHERE ql.published = 1'
+                )
+                full_response.push(questions_in_lec)
+            }
+            catch (e) {
+                // TODO: send back better error msg using exception 'e' (/lib/string_helpers.js - serializeSequelizeErrors())
+                res.status(400).send({error: "Unable to get lecture or questions"})
+            }
+            // TODO: add check if lecture has ever been published in the past? is there a database field to check that?
+            // TODO: add check if the full_response is empty
+            res.status(200).json(full_response)   // all good, return updated lecture object
+        }
+    }
+})
+
+// delete lecture and ALL relations to this lecture
+router.delete('/:lecture_id', async function (req, res) {
+    const user = req.user
+    const lectureId = req.params.lecture_id
+    const enrollment = getEnrollment(user.id, courseId)
+
+    if (enrollment.role != 'teacher') {
+        res.status(403).send()
+    }
+    else {  // if user is a teacher
+        try {
+            await db.Lecture.destroy({  // delete from lecture table
+                where: {
+                    id: lectureId
+                }
+            })
+            await db.QuestionInLecture.destroy({    // delete from lecture-question relationship
+                where: {
+                    lectureId: lectureId
+                }
+            })
+            await db.LectureForSection.destroy({    // delete from lecture-section relationship
+                where: {
+                    lectureId: lectureId
+                }
+            })
+        }
+        catch (e) {
+            res.status(500).send()
+        }
     }
 })
 
